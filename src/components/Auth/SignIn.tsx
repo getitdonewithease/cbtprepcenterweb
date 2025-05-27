6d1a610a0147dc9c40723beb0506efd145f4ffa1
= <108731105+ZafrullahId@users.noreply.github.com>
2025-05-27 19:40:02 +0100

    Refactor Home component to fetch user profile and recent test data from API; implement subject performance analysis display and handle loading/error states.

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -27,7 +27,7 @@ const SignIn = () => {
 
       // Store token in localStorage
       localStorage.setItem("token", data.accessToken);
-
+      localStorage.setItem("tokenExpiration", data.expirationTime);
       // Redirect to home page on success
       window.location.href = "/";
     } catch (err: any) {
